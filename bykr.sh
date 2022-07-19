2830c870de83842d2c49831f510322f77cd16b05
yaswanth2000reddy <yaswanthkumarreddy2000bykr@gmail.com>
2022-07-19 13:01:03 +0000

    updated bykr.sh

--- bykr.sh
+++ bykr.sh
@@ -3,3 +3,4 @@ asdfghjklZxcvbnmertyuiop
 wertyuiopasdfghjkzxcvbnm,qwertyuio
 asdfghjklxcvbnm,cvbnm,
 asdfghsdtyudfghfyaswanth
+others add to this file common that add tothe common man of the this normal comommand to the same to the
