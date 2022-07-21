96a90bf9658afceefd9e614a275f24c41aec5376
yaswanth2000reddy <108867858+yaswanth2000reddy@users.noreply.github.com>
2022-07-21 11:28:51 +0530

    Update bykr.sh

--- bykr.sh
+++ bykr.sh
@@ -5,3 +5,4 @@ asdfghjklxcvbnm,cvbnm,
 asdfghsdtyudfghfyaswanth
 others add to this file common that add tothe common man of the this normal comommand to the same to the
 it commonly in stage branch
+yaswanth reddy
